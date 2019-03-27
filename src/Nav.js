6ebf2e6638c93d9ad6edcbd1f301efa9b2b83eac
Garyun Young <garyunyoung@gmail.com>
2019-03-27 14:56:34 +0000

    Add header + nav layout

--- src/Nav.js
+++ src/Nav.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import './Nav.scss'
+import React from "react";
+import "./Nav.scss";
 
 export default class Nav extends React.Component {
-    render () {
-        return (
-            <div className='nav'>
-                test
-            </div>
-        )
-    }
-
-
+  render() {
+    return (
+      <section className="header">
+        <h1 className="header__title">garyun young</h1>
+        <div>
+          <ul className="nav">
+            <li className="nav__item">projects</li>
+            <li className="nav__item">about</li>
+            <li className="nav__item">cv</li>
+          </ul>
+        </div>
+      </section>
+    );
+  }
 }
