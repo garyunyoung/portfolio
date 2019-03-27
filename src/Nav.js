e83f7ee0dd23ff27c35215b649a00643e8a52c8f
Garyun Young <garyunyoung@gmail.com>
2019-03-27 13:16:16 +0000

    Add node-sass

---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Nav.scss'
+
+export default class Nav extends React.Component {
+    render () {
+        return (
+            <div className='nav'>
+                test
+            </div>
+        )
+    }
+
+
+}
