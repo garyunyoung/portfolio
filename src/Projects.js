e75fbef585b546afda787221b4430b41c662a0be
Garyun Young <garyunyoung@gmail.com>
2019-03-28 11:11:39 +0000

    Add web portfolio pictures - beths + skilaa

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import splore from "./images/splore-2018.jpg";
+import splore from "./images/splore-c.jpg";
 import nats from "./images/night-at-the-savoy.jpg";
 import scj from "./images/sail-city-jump.jpg";
+import beths from './images/the-beths.jpg';
+import skilaa from './images/skilaa.jpg';
 import "./Projects.scss";
 
 export default class Project extends React.Component {
@@ -10,8 +12,10 @@ export default class Project extends React.Component {
     this.state = {
       projects: [
         { img: splore, title: "splore 2018", category: "pr" },
-        { img: nats, title: "night at the savoy", category: "web" },
-        { img: scj, title: "sail city jump", category: "pr" }
+        { img: nats, title: "night at the savoy", category: "pr" },
+        { img: scj, title: "sail city jump", category: "pr" },
+        { img: beths, title: "the beths", category: "web" },
+        { img: skilaa, title: "skilaa", category: "web" }
       ],
       filter: "all"
     };
@@ -28,7 +32,7 @@ export default class Project extends React.Component {
       <section className="projects">
         <h1 className="projects__title">projects</h1>
         <ul className="projects__nav">
-          <li className="projects__nav__item" onClick={() => {this.setFilter('web')}}>web development</li>
+          <li className="projects__nav__item" onClick={() => {this.setFilter('web')}}>web</li>
           <li className="projects__nav__item" onClick={() => {this.setFilter('pr')}}>publicity / pr</li>
           <li className="projects__nav__item" onClick={() => {this.setFilter('all')}}>all</li>
         </ul>
