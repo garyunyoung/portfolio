64b184f25560184d06a032b28fe836ebc96be181
Garyun Young <garyunyoung@gmail.com>
2019-03-30 08:49:01 +0000

    Add about profile picture

--- src/About.js
+++ src/About.js
@@ -1,10 +1,11 @@
 import React from "react";
+import profilePic from './images/profile-pic.jpg';
 import "./About.scss";
 
 export default function About() {
   return (
     <section className="about">
-      <h1 className="projects__title">about</h1>
+      <img className="about__photo" src={profilePic}/>
     </section>
   );
 }
