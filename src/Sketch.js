195141aa1b09b6a852c9bc4fee685ac5643be23c
Garyun Young <garyunyoung@gmail.com>
2020-01-05 12:28:25 +0000

    Add tasks to readme

--- src/Sketch.js
+++ src/Sketch.js
@@ -12,6 +12,9 @@ export default class Contact extends Component {
     height: "100%"
   };
 
+  circles = [];
+  numOfCircles = 13;
+
   setup = (p5, canvasParentRef) => {
     p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
   };
@@ -19,9 +22,9 @@ export default class Contact extends Component {
   draw = p5 => {
     // p5.fill(0,50,80);
     // p5.fill(0,20,80);
-    p5.fill(90,10,90);
-    p5.stroke(255);
-    p5.ellipse(0, 0, 0, 0);
+    // p5.fill(90,10,90);
+    // p5.stroke(255);
+    // p5.ellipse(0, 0, 0, 0);
   };
 
   mouseMoved = p5 => {
