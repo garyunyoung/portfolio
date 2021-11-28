55e5f4273efddf32abd219258afec1fcab13d3cc
Garyun Young <garyun@stitched.co.uk>
2021-11-28 18:28:35 +1300

    Add favourite learning resources to bottom of about section, Tidy about section by extracting link items to reusable component

--- src/About.js
+++ src/About.js
@@ -5,32 +5,50 @@ export default function About() {
   return (
     <section id="about" className="about">
       <div className="about__content">
-        <div className="about__text-container">
-          <p className="about__header">Nice to meet you!</p>
-          <div className="about__text">
-            <p>I'm a front-end developer who is interested in contributing towards high-quality web and mobile experiences for anyone and everyone.</p>
-            <br />
-            <p>Things I like:</p>
-            <br />
-            <ul className="about__list">
-              <li>User-centered design</li>
-              <li>Testing and iterating - two-way feedback</li>
-              <li>Writing clear, reusuable, and reliable code</li>
-              <li>Accessibility (A11Y) advocate</li>
-              <li>Diversity and inclusivity</li>
-              <li>Being a 'net-positive' to the world ✌️</li>
-            </ul>
-          </div>
-          <div className="about__links">
-            <a className="about__link"
-              href="https://res.cloudinary.com/garyou/image/upload/v1637715764/portfolio/Garyun_Young_Developer_CV_2021.pdf"
-              target='_blank'
-              rel='noopener noreferrer'
-            >See resume</a>
-            <a className="about__link" href="mailto:garyunyoung@gmail.com">Email me</a>
-          </div>
+        <p className="about__header">Nice to meet you!</p>
+        <p className="about__description">I'm a front-end developer who is interested in contributing towards high-quality web and mobile experiences for anyone and everyone.</p>
+        <div className="about__personality-list-container">
+          <p className="about__list-header">Things I like:</p>
+          <ul className="about__personality-list">
+            <li>User-centered design</li>
+            <li>Testing, iterating, and collaboration</li>
+            <li>Writing clear, reusuable, and reliable code</li>
+            <li>Diversity and inclusivity</li>
+            <li>Being a 'net-positive' to the world ✌️</li>
+          </ul>
+        </div>
+        
+        <div className="about__links">
+          <a className="about__link"
+            href="https://res.cloudinary.com/garyou/image/upload/v1637715764/portfolio/Garyun_Young_Developer_CV_2021.pdf"
+            target='_blank'
+            rel='noopener noreferrer'
+          >See resume</a>
+          <a className="about__link" href="mailto:garyunyoung@gmail.com">Email me</a>
+        </div>
+
+        <div className="about__resource-list-container">
+          <p className="about__list-header">Favourite learning resources:</p>
+          <ul className="about__resource-list">
+            <ListItemLink linkName="The Bike Shed" link="https://www.bikeshed.fm/" />
+            <ListItemLink linkName="Syntax" link="https://syntax.fm/" />
+            <ListItemLink linkName="Execute Program" link="https://www.executeprogram.com/" />
+            <ListItemLink linkName="The A11Y Project" link="https://www.a11yproject.com/" />
+          </ul>
         </div>
       </div>
     </section>
   );
+}
+
+function ListItemLink(props) {
+  return (
+    <li className="about__resource-list-item">
+      <a className="about__resource-list-item-link"
+        href={props.link}
+        target='_blank'
+        rel='noopener noreferrer'
+      >{props.linkName}</a>
+    </li>
+  )
 }
