a40fb8d0ba104d8290a77acb3a37837a8da72a92
Garyun Young <garyunyoung@gmail.com>
2021-12-06 12:05:37 +1300

    Add conditional to element in view, Make note to refactor

--- src/utilities/ElementInView.js
+++ src/utilities/ElementInView.js
@@ -1,26 +1,29 @@
 export default function elementInView() {
-  const projects = document.querySelector('#projects');
-  const projectsEl = document.querySelector('.site-menu__nav-item-link--projects');
-  const about = document.querySelector('#about');
-  const aboutEl = document.querySelector('.site-menu__nav-item-link--about');
+  if (document.querySelector('#projects') && document.querySelector('#about')) {
+    const projects = document.querySelector('#projects')
+    const projectsEl = document.querySelector('.site-menu__nav-item-link--projects')
+    const about = document.querySelector('#about')
+    const aboutEl = document.querySelector('.site-menu__nav-item-link--about')
 
-  const projectsPos = projects.getBoundingClientRect().top + window.scrollY
-  const aboutPos = about.getBoundingClientRect().top + window.scrollY
+    const projectsPos = projects.getBoundingClientRect().top + window.scrollY
+    const aboutPos = about.getBoundingClientRect().top + window.scrollY
 
-  window.addEventListener('scroll', () => {
-    const scrolled = window.scrollY;
+    window.addEventListener('scroll', () => {
+      const scrolled = window.scrollY
 
-    if (scrolled < projectsPos) {
-      projectsEl.classList.remove('is-active')
-      aboutEl.classList.remove('is-active')
+      if (scrolled < projectsPos) {
+        projectsEl.classList.remove('is-active')
+        aboutEl.classList.remove('is-active')
 
-    } else if (scrolled > aboutPos) {
-      aboutEl.classList.add('is-active')
-      projectsEl.classList.remove('is-active')
+      } else if (scrolled > aboutPos) {
+        aboutEl.classList.add('is-active')
+        projectsEl.classList.remove('is-active')
 
-    } else {
-      projectsEl.classList.add('is-active')
-      aboutEl.classList.remove('is-active')
-    }
-  })
+      } else {
+        projectsEl.classList.add('is-active')
+        aboutEl.classList.remove('is-active')
+      }
+    })
+
+  }
 }
