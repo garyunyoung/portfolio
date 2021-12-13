7dbae10d3eb7b8221212b0b58f4467648d3e1e6d
Garyun Young <garyunyoung@gmail.com>
2021-12-14 10:13:15 +1300

    Extract NavItemLink from navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,6 @@ import githubLogo from '../assets/logos/github.svg';
 import codewarsLogo from '../assets/logos/codewars.svg';
 import exercismLogo from '../assets/logos/exercism.svg';
 
-
 import '../styles/Navigation.scss';
 
 export default function Navigation() {
@@ -62,54 +61,52 @@ export default function Navigation() {
   );
 }
 
-function NavigationMenu(props) {
+function NavigationMenu({ isSocialsOpen, toggleSocials, closeNav, isInScrollView }) {
   return (
     <nav className='navigation-menu navigation__nav'>
       <ul className='navigation-menu__nav-items'>
         <span className="navigation-menu__nav-main-items">
-          <li className='navigation-menu__nav-item'>
-            <a className={`navigation-menu__nav-item-link ${props.isInScrollView === 'projects' ? 'is-active' : ""}`} href='/#projects' onClick={props.closeNav}>
-              Projects
-            </a>
-          </li>
-          <li className='navigation-menu__nav-item'>
-            <a className={`navigation-menu__nav-item-link ${props.isInScrollView === 'about' ? 'is-active' : ""}`} href='/#about' onClick={props.closeNav}>
-              About
-            </a>
-          </li>
+          <NavItemLink
+            link="/#projects"
+            title="Projects"
+            modifier='projects'
+            closeNav={closeNav}
+            isInScrollView={isInScrollView} />
+
+          <NavItemLink
+            link="/#about"
+            title="About"
+            modifier='about'
+            closeNav={closeNav}
+            isInScrollView={isInScrollView} />
         </span>
-        <span className='navigation-menu__nav-socials-container'>
+        <span className='navigation-menu__nav-code-container'>
           <li
-            onClick={props.toggleSocials}
+            onClick={toggleSocials}
           >
             <button className={`
               navigation-menu__nav-item-link 
               navigation-menu__nav-item-link--socials 
-              ${props.isSocialsOpen ? 'is-selected is-active' : ''}
+              ${isSocialsOpen ? 'is-selected is-active' : ''}
             `}>
               Code
             </button>
           </li>
-          <span className={`navigation-menu__nav-socials ${props.isSocialsOpen ? 'is-visible' : ''}`}>
-            <li>
-              <NavItemLinkSocial
-                link="https://github.com/garyunyoung"
-                title="Github"
-                image={githubLogo} />
-            </li>
-            <li>
-              <NavItemLinkSocial
-                link="https://exercism.org/profiles/garyunyoung"
-                title="Exercism"
-                image={exercismLogo} />
+          <span className={`navigation-menu__nav-socials ${isSocialsOpen ? 'is-visible' : ''}`}>
+            <NavItemLinkSocial
+              link="https://github.com/garyunyoung"
+              title="Github"
+              image={githubLogo} />
 
-            </li>
-            <li>
-              <NavItemLinkSocial
-                link="https://www.codewars.com/users/garryunn"
-                title="Codewars"
-                image={codewarsLogo} />
-            </li>
+            <NavItemLinkSocial
+              link="https://exercism.org/profiles/garyunyoung"
+              title="Exercism"
+              image={exercismLogo} />
+
+            <NavItemLinkSocial
+              link="https://www.codewars.com/users/garryunn"
+              title="Codewars"
+              image={codewarsLogo} />
           </span>
         </span>
       </ul>
@@ -117,20 +114,33 @@ function NavigationMenu(props) {
   );
 }
 
-function NavItemLinkSocial(props) {
+function NavItemLink(props) {
   return (
-    <a
-      className="navigation-menu__nav-item-link-social"
-      href={props.link}
-      target='_blank'
-      rel='noopener noreferrer'
-    >
-      <p className="navigation-menu__nav-item-text-social">{props.title}</p>
-      <img src={props.image} alt='' />
-    </a>
+    <li className='navigation-menu__nav-item'>
+      <a
+        className={`navigation-menu__nav-item-link ${props.isInScrollView === props.modifier ? 'is-active' : ""}`}
+        href={props.link}
+        onClick={props.closeNav}>
+        {props.title}</a>
+    </li>
   )
 }
 
+function NavItemLinkSocial(props) {
+  return (
+    <li>
+      <a
+        className="navigation-menu__nav-item-link-social"
+        href={props.link}
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <p className="navigation-menu__nav-item-text-social">{props.title}</p>
+        <img src={props.image} alt='' />
+      </a>
+    </li>
+  )
+}
 
 function elementInView(setIsInScrollView) {
   const projects = document.querySelector('#projects')
